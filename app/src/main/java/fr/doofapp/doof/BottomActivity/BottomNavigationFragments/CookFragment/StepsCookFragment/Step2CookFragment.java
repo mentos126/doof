812b621d8ba63b2c1b455e031bf7c2a55386ea10
henri <henrique0126@gmail.com>
2018-05-02 15:25:18 +0200

    add minor change

--- app/src/main/java/fr/doofapp/doof/BottomActivity/BottomNavigationFragments/CookFragment/StepsCookFragment/Step2CookFragment.java
+++ app/src/main/java/fr/doofapp/doof/BottomActivity/BottomNavigationFragments/CookFragment/StepsCookFragment/Step2CookFragment.java
@@ -4,11 +4,17 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import fr.doofapp.doof.R;
 
@@ -19,6 +25,12 @@ public class Step2CookFragment extends Fragment {
     Button next;
     String date;
     String time;
+    String adress;
+    CheckBox checkAdress;
+    CheckBox checkNewAdress;
+    Button modify;
+    LinearLayout rel;
+    EditText new_adress;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -32,27 +44,85 @@ public class Step2CookFragment extends Fragment {
         Log.e("======DATE======",date);
         Log.e("======TIME======",time);
 
+        checkAdress  = (CheckBox) rootView.findViewById(R.id.check_adress);
+        rel = (LinearLayout) rootView.findViewById(R.id.rel);
+        checkNewAdress = (CheckBox) rootView.findViewById(R.id.check_new_dress);
+
+        new_adress = (EditText) rootView.findViewById(R.id.new_adress);
+        new_adress.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                checkNewAdress.setText(editable);
+            }
+        });
+
+        modify = (Button) rootView.findViewById(R.id.add_new_adress);
+        modify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(rel.getVisibility() == View.VISIBLE){
+                    checkNewAdress.setChecked(false);
+                    rel.setVisibility(View.INVISIBLE);
+                }else{
+                    rel.setVisibility(View.VISIBLE);
+                    modify.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
         next = (Button) rootView.findViewById(R.id.next);
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                //String d = date.getText().toString();
+                if(checkAdress.isChecked() && checkNewAdress.isChecked()){
+                    Toast.makeText(getActivity(), R.string.prompt_select_adress,Toast.LENGTH_LONG).show();
+                }else if(!checkAdress.isChecked() && !checkNewAdress.isChecked()) {
+                    Toast.makeText(getActivity(), R.string.prompt_select_prompt_only_one,Toast.LENGTH_LONG).show();
+                }else if(checkAdress.isChecked()){
+                    adress = checkAdress.getText().toString();
+                    Bundle bundle = new Bundle();
+                    bundle.putString("Date", date);
+                    bundle.putString("Time", time);
+                    bundle.putString("Adress",adress);
 
-                Bundle bundle = new Bundle();
-                bundle.putString("Date", date);
-                bundle.putString("Time", time);
+                    FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
-                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
-                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                    Step3CookFragment step3CookFragment = new Step3CookFragment();
+                    step3CookFragment.setArguments(bundle);
 
-                Step3CookFragment step3CookFragment = new Step3CookFragment();
-                step3CookFragment.setArguments(bundle);
+                    Log.e("====COMMIT====","=======COMIT====");
+                    fragmentTransaction.replace(R.id.frame_cook_container, step3CookFragment);
+                    fragmentTransaction.commit();
+                }else{
+                    adress = checkNewAdress.getText().toString();
+                    Bundle bundle = new Bundle();
+                    bundle.putString("Date", date);
+                    bundle.putString("Time", time);
+                    bundle.putString("Adress",adress);
 
-                Log.e("====COMMIT====","=======COMIT====");
+                    FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
-                fragmentTransaction.replace(R.id.frame_cook_container, step3CookFragment);
-                fragmentTransaction.commit();
+                    Step3CookFragment step3CookFragment = new Step3CookFragment();
+                    step3CookFragment.setArguments(bundle);
+
+                    Log.e("====COMMIT====","=======COMIT====");
+                    fragmentTransaction.replace(R.id.frame_cook_container, step3CookFragment);
+                    fragmentTransaction.commit();
+                }
+
 
             }
         });
--- app/src/main/java/fr/doofapp/doof/BottomActivity/BottomNavigationFragments/CookFragment/StepsCookFragment/Step3CookFragment.java
+++ app/src/main/java/fr/doofapp/doof/BottomActivity/BottomNavigationFragments/CookFragment/StepsCookFragment/Step3CookFragment.java
@@ -1,7 +1,11 @@
 package fr.doofapp.doof.BottomActivity.BottomNavigationFragments.CookFragment.StepsCookFragment;
 
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
@@ -10,18 +14,40 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.NumberPicker;
+import android.widget.Toast;
+
+import com.nbsp.materialfilepicker.MaterialFilePicker;
+import com.nbsp.materialfilepicker.ui.FilePickerActivity;
+
+import java.io.ByteArrayOutputStream;
 
 import fr.doofapp.doof.R;
+
+import static android.app.Activity.RESULT_OK;
 
 public class Step3CookFragment extends Fragment {
     View rootView;
     NumberPicker np;
-    NumberPicker price;
+    NumberPicker npPrice;
     Button next;
+    EditText name;
+    EditText description;
+    ImageView photo;
+    Button takePhoto;
+    Button searchPhoto;
+    CheckBox contain;
+
+    int price;
+    int nbPortion;
+    Bitmap newPhoto;
 
     String date;
     String time;
+    String adress;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -31,9 +57,32 @@ public class Step3CookFragment extends Fragment {
 
         date = bundle.get("Date").toString();
         time = bundle.get("Time").toString();
+        adress = bundle.get("Adress").toString();
 
         Log.e("======DATE=====",date);
         Log.e("======TIME=====",time);
+        Log.e("======Adress=====",adress);
+
+        name = (EditText) rootView.findViewById(R.id.name);
+        description = (EditText) rootView.findViewById(R.id.description);
+        photo = (ImageView) rootView.findViewById(R.id.prompt_photo);
+        contain  = (CheckBox) rootView.findViewById(R.id.is_container);
+
+        takePhoto = (Button) rootView.findViewById(R.id.take_photo);
+        takePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                actionButtonTakePhoto();
+            }
+        });
+
+        searchPhoto = (Button) rootView.findViewById(R.id.search_photo);
+        searchPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                actionButtonSearchFile();
+            }
+        });
 
         np = (NumberPicker) rootView.findViewById(R.id.nbPortion);
         np.setMinValue(0);
@@ -42,18 +91,18 @@ public class Step3CookFragment extends Fragment {
         np.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
             @Override
             public void onValueChange(NumberPicker picker, int oldVal, int newVal){
-                Log.e("======NP=======", newVal+"");
+                nbPortion = newVal;
             }
         });
 
-        price = (NumberPicker) rootView.findViewById(R.id.nbPortion);
-        price.setMinValue(0);
-        price.setMaxValue(50);
-        price.setWrapSelectorWheel(true);
-        price.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+        npPrice = (NumberPicker) rootView.findViewById(R.id.price);
+        npPrice.setMinValue(0);
+        npPrice.setMaxValue(50);
+        npPrice.setWrapSelectorWheel(true);
+        npPrice.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
             @Override
             public void onValueChange(NumberPicker picker, int oldVal, int newVal){
-                Log.e("======NP=======", newVal+"");
+                price = newVal;
             }
         });
 
@@ -62,27 +111,90 @@ public class Step3CookFragment extends Fragment {
             @Override
             public void onClick(View view) {
 
-                //String d = date.getText().toString();
+                String title = name.getText().toString();
+                String desc = description.getText().toString();
 
-                Bundle bundle = new Bundle();
-                bundle.putString("Date", date);
-                bundle.putString("Time", time);
+                if(nbPortion > 0 && !desc.equals("") && !title.equals("")){
+                    if(newPhoto != null){
+                        Boolean ct = contain.isChecked();
 
-                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
-                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                        newPhoto.compress(Bitmap.CompressFormat.PNG, 100, stream);
+                        byte[] byteArray = stream.toByteArray();
 
-                Step5CookFragment step5CookFragment = new Step5CookFragment();
-                step5CookFragment.setArguments(bundle);
+                        Bundle bundle = new Bundle();
+                        bundle.putString("Date", date);
+                        bundle.putString("Time", time);
+                        bundle.putString("Adress", adress);
+                        bundle.putString("Title", title);
+                        bundle.putString("Description", desc);
+                        bundle.putByteArray("Photo",byteArray);
+                        bundle.putInt("NbPoriotn", nbPortion);
+                        bundle.putInt("Price", price);
+                        bundle.putBoolean("IsContainer",ct);
 
-                Log.e("====COMMIT====","=======COMIT====");
+                        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
-                fragmentTransaction.replace(R.id.frame_cook_container, step5CookFragment);
-                fragmentTransaction.commit();
+                        Step5CookFragment step5CookFragment = new Step5CookFragment();
+                        step5CookFragment.setArguments(bundle);
+
+                        Log.e("====COMMIT====","=======COMIT====");
+
+                        fragmentTransaction.replace(R.id.frame_cook_container, step5CookFragment);
+                        fragmentTransaction.commit();
+                    }else{
+                        Toast.makeText(getActivity(), R.string.prompt_add_photo,Toast.LENGTH_LONG).show();
+                    }
+                }else{
+                    if(!desc.equals("") && !title.equals("")){
+                        Toast.makeText(getActivity(),getResources().getString(R.string.prompt_edit_all),Toast.LENGTH_LONG).show();
+                    }else{
+                        Toast.makeText(getActivity(), R.string.prompt_portion,Toast.LENGTH_LONG).show();
+                    }
+                }
 
             }
         });
 
         return rootView;
     }
+
+    protected void actionButtonSearchFile(){
+        new MaterialFilePicker()
+                .withSupportFragment(this)
+                .withRequestCode(1)
+                //.withFilter(Pattern.compile(".*\\.jpg$")) // Filtering files and directories by file name using regexp
+                .withFilterDirectories(false) // Set directories filterable (false by default)
+                .withHiddenFiles(false) // Show hidden files and folders
+                .start();
+    }
+
+    protected void actionButtonTakePhoto(){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent,0);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // take photo
+        if (requestCode == 0 && resultCode == RESULT_OK) {
+            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+            newPhoto = bitmap;
+            photo.setImageBitmap(bitmap);
+        }
+        //search file
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            Log.e("=====SEARCH=====","======SEACHR===");
+            String filePath = data.getStringExtra(FilePickerActivity.RESULT_FILE_PATH);
+            BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+            Bitmap b = BitmapFactory.decodeFile(filePath ,bmOptions);
+            newPhoto = b;
+            photo.setImageBitmap(b);
+        }
+    }
+
 
 }
